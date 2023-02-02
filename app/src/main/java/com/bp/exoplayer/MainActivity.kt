f16a87680e892023e539aed45e97abd5f5afb837
Behrooz <pahlavan111@gmail.com>
2023-02-03 00:18:37 +0330

    Add PlayerView to MainActivity.kt

--- app/src/main/java/com/bp/exoplayer/MainActivity.kt
+++ app/src/main/java/com/bp/exoplayer/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.bp.exoplayer
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.media3.exoplayer.ExoPlayer
+import com.bp.exoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private var player: ExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initialExoPlayer()
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    fun initialExoPlayer() {
+        player = ExoPlayer.Builder(this)
+            .build()
+            .also {
+                binding.playerView.player = it
+            }
     }
 }
