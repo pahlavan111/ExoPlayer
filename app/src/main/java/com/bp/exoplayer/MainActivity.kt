6fe3c8d3f1cdfc344557dd908e531225167b7508
Behrooz <pahlavan111@gmail.com>
2023-02-05 00:14:18 +0330

    handle exoplayer last state and add media Item

--- app/src/main/java/com/bp/exoplayer/MainActivity.kt
+++ app/src/main/java/com/bp/exoplayer/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.bp.exoplayer
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.Util
 import androidx.media3.exoplayer.ExoPlayer
 import com.bp.exoplayer.databinding.ActivityMainBinding
 
@@ -11,21 +15,76 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    private var player: ExoPlayer? = null
+    private var exoPlayer: ExoPlayer? = null
+
+    private var playWhenReady = true
+    private var currentItem = 0
+    private var playBackPosition = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        initialExoPlayer()
     }
 
-    @SuppressLint("UnsafeOptInUsageError")
-    fun initialExoPlayer() {
-        player = ExoPlayer.Builder(this)
+    private fun initialExoPlayer() {
+        exoPlayer = ExoPlayer.Builder(this)
             .build()
-            .also {
-                binding.playerView.player = it
+            .also { exoPlayer ->
+                binding.playerView.player = exoPlayer
+
+                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3_2))
+                exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentItem, playBackPosition)
+                exoPlayer.prepare()
             }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initialExoPlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT <= 23 || exoPlayer == null) {
+            initialExoPlayer()
+        }
+    }
+
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, binding.playerView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.let {
+            playBackPosition = it.currentPosition
+            currentItem = it.currentMediaItemIndex
+            playWhenReady = it.playWhenReady
+            it.release()
+        }
+        exoPlayer = null
     }
 }
